3f24e4188e4b5e7cd87badc955124139f73e93ed
jf0o <46459398+jf0o@users.noreply.github.com>
2019-01-31 10:26:27 -1000

    Completed Swift Playrgounds Two

---
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var goodGrade = "I've been doing all my work"
+if goodGrade == "I've been doing all my work"
+{
+print("I should be getting an A at this point")
+}
+else
+{
+    print("I need to work harder in this class")
+}
+var gradeA = true
+if gradeA{
+    //if code block
+    print("I really like this class")
+}
+else
+{
+    //else code block
+    print("I'm not so sure about coding")
+}
